5aade9ecd13daf074277b484ee688f3a49920bcb
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-03-28 20:52:57 -0300

    Todo controller and index creation

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Context;
 
 namespace ToDo.Controllers;
 public class TodoController : Controller
 {
+    private readonly TodoTaskContext _context;
+
+    public TodoController(TodoTaskContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        //var contatos = _context.TodoTasks.AsNoTracking().ToList();
         return View();
     }
 }
