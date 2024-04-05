524deee05bbcb976298b26ccefb58ed0bd638182
Vinícius <viniciusdosanjos63@gmail.com>
2024-04-05 19:54:19 -0300

    asp-for attribute fix

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -18,10 +18,12 @@ public class TodoController : Controller
         var tasks = _context.TodoTasks.AsNoTracking().ToList();
         return View(tasks);
     }
+
     public IActionResult Create()
     {
         return View();
     }
+
     [HttpPost]
     public IActionResult Create(TodoTask task)
     {
@@ -31,5 +33,29 @@ public class TodoController : Controller
             _context.SaveChanges();
         }
         return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var task = _context.TodoTasks.Find(id);
+        if (task is null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(task);
+    }
+
+    [HttpPut]
+    public IActionResult Edit(TodoTask task)
+    {
+        if (task is null) 
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        _context.Entry(task).State =
+            Microsoft.EntityFrameworkCore.EntityState.Modified;
+        _context.SaveChanges();
+        return View(task);
     }
 }
--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
 
 app.Run();
