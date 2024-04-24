12a5eaced789a7e3779d995603b14d11a22809b8
Vinícius <viniciusdosanjos63@gmail.com>
2024-04-23 23:40:16 -0300

    IRepository added

---
+++ Repositories/ITodoTaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using ToDo.Models;
+
+namespace ToDo.Repositories;
+
+public interface ITodoTaskRepository
+{
+    TodoTask GetById(int id);
+    IEnumerable<TodoTask> GetAll();
+    TodoTask Add(TodoTask task);
+    TodoTask Update(TodoTask task);
+    TodoTask Delete(int id);
+}
